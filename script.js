6f76819a6ff39e78f5e4559da4f933028c28227d
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-19 12:55:25 +0100

    Light Mode Function

--- script.js
+++ script.js
@@ -15,7 +15,17 @@ function darkMode() {
     imageOne.src = 'img/undraw_proud_coder_dark.svg';
     imageTwo.src = 'img/undraw_feeling_proud_dark.svg';
     imageThree.src = 'img/undraw_conceptual_idea_dark.svg';
-
+}
+//Light mode 
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.remove('fa-moon');
+    toggleIcon.children[1].classList.add('fa-sun');
+    imageOne.src = 'img/undraw_proud_coder_light.svg';
+    imageTwo.src = 'img/undraw_feeling_proud_light.svg';
+    imageThree.src = 'img/undraw_conceptual_idea_light.svg';
 }
 //Switch theme 
 function switchTheme(event) {
