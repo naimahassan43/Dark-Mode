b445d1cf81b50cba33091d76fe5bb999e2c6bfff
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-19 12:10:07 +0100

    variable declaration

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const toggleSwitch = document.querySelector('input[type = "checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.querySelector('.toggle-icon');
+const imageOne = document.getElementById('image1');
+const imageTwo = document.getElementById('image2');
+const imageThree = document.getElementById('image3');
+const testBox = document.getElementById('test-box');
 
 //Switch theme 
 function switchTheme(event) {
     const target = event.target.checked;
+    if (target == true) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
+    }
 }
 //Events 
 
