6d8db83b2cb8e8bcc1029adbe833ace8490b3723
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-19 12:50:07 +0100

    Dark Mode Function

--- script.js
+++ script.js
@@ -4,8 +4,19 @@ const toggleIcon = document.querySelector('.toggle-icon');
 const imageOne = document.getElementById('image1');
 const imageTwo = document.getElementById('image2');
 const imageThree = document.getElementById('image3');
-const testBox = document.getElementById('test-box');
+const textBox = document.getElementById('text-box');
+//Dark mode 
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.remove('fa-sun');
+    toggleIcon.children[1].classList.add('fa-moon');
+    imageOne.src = 'img/undraw_proud_coder_dark.svg';
+    imageTwo.src = 'img/undraw_feeling_proud_dark.svg';
+    imageThree.src = 'img/undraw_conceptual_idea_dark.svg';
 
+}
 //Switch theme 
 function switchTheme(event) {
     const target = event.target.checked;
